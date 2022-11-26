bbeaa62517ad2c8c7f9899426d02515a1a788425
asem ayoub <asemayoub2011@gmail.com>
2022-11-26 23:08:44 +0200

    update1

--- lib/view/auth/login_screen.dart
+++ lib/view/auth/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:ecommercegetx/view/widget/Custom_Components.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
+
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,13 +11,16 @@ class LoginScreen extends StatelessWidget {
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.only(
+
           right: 20,
           left: 20,
           top: 20,
+
         ),
         child: Column(
           children: [
             Container(
+
               padding: EdgeInsets.only(
                 top: 20,
                 right: 10,
@@ -24,37 +28,45 @@ class LoginScreen extends StatelessWidget {
               ),
               color: Colors.white,
               child: Column(
+
+
                 crossAxisAlignment: CrossAxisAlignment.start,
+
                 children: [
                   // Welcome And Sign Up
 
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
                     children: [
+
                       CustomText(
                         text: 'Welcome,',
                         fontsize: 30,
-                        fontWeight: FontWeight.bold,
+                        fontWeight:FontWeight.bold ,
                         color: Colors.black,
+
                       ),
                       CustomText(
                         text: 'SignUp',
                         fontsize: 18,
-                        fontWeight: FontWeight.w400,
+                        fontWeight:FontWeight.w400 ,
                         color: Primarycolor,
+
                       ),
+
+
                     ],
+
                   ),
-                  SizedBox(
-                    height: 5,
-                  ),
+                  SizedBox(height: 5,),
                   CustomText(
-                    text: 'Sign in to Continue',
+                    text: 'Sign in to Continue' ,
                     fontsize: 14,
                     color: Colors.grey,
                   ),
                   SizedBox(
-                    height: 56,
+                    height:56 ,
                   ),
                   InputForm(
                     type: TextInputType.emailAddress,
@@ -62,11 +74,11 @@ class LoginScreen extends StatelessWidget {
                     color: Colors.grey,
                     iconPrefex: Icons.email_outlined,
                     hint: 'email@example.com',
-                    onsave: (value) {},
-                    onvalidate: (value) {},
+                    onsave: (value){},
+                    onvalidate: (value){},
                   ),
                   SizedBox(
-                    height: 40,
+                    height:40 ,
                   ),
                   InputForm(
                     type: TextInputType.visiblePassword,
@@ -75,18 +87,21 @@ class LoginScreen extends StatelessWidget {
                     iconPrefex: Icons.lock,
                     iconSuffex: Icons.visibility,
                     hint: '**************',
-                    onsave: (value) {},
-                    onvalidate: (value) {},
+                    onsave: (value){},
+                    onvalidate: (value){},
                   ),
-                  SizedBox(
-                    height: 15,
-                  ),
+                  SizedBox(height: 15,),
                   CustomText(
                     text: 'Forget Password?',
                     fontsize: 14,
                     alignment: Alignment.topRight,
                   ),
-                  FlatButton(onPressed: () {}, child: CustomText()),
+                  FlatButton(
+                      onPressed: (){},
+                      child: CustomText()
+                  ),
+
+
                 ],
               ),
             ),
